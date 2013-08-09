3f07288666b8e3f4c151f5a2607e71b63593375d
ebemunk <ebemunk@gmail.com>
2013-08-09 09:19:25 -0700

    separate makefile into two for variables

--- Makefile-variables.mk
+++ Makefile-variables.mk
@@ -35,19 +35,3 @@ LDFLAGS = -L$(OCV_LIB_STATIC_ROOT)\3rdparty\lib \
 		-static
 EXE_NAME = phoenix.exe
 endif
-
-all: phoenix.o functions.o resources.o
-	$(CC) phoenix.o functions.o resources.o $(CFLAGS) $(LDFLAGS) -o $(EXE_NAME)
-
-phoenix.o: phoenix.cpp functions.o
-	$(CC) -c phoenix.cpp $(INC_PATHS)
-
-functions.o: functions.cpp
-	$(CC) -c functions.cpp -I$(OCV_INC)
-
-resources.o: resources.rc
-	windres resources.rc resources.o
-
-.PHONY: clean
-clean:
-	rm *.o phoenix.exe phoenix-dyn.exe
