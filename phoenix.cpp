3e80c4ef653f99fbea8efc2d3be09ac23c9daf95
ebemunk <ebemunk@gmail.com>
2013-10-15 02:40:14 -0700

    refactor out code to run_analysis func

--- phoenix.cpp
+++ phoenix.cpp
@@ -21,6 +21,71 @@ using namespace boost::program_options;
 using namespace boost::filesystem;
 using boost::property_tree::ptree;
 
+enum analysis_type {A_ELA, A_LG, A_AVGDIST, A_HSV, A_LAB};
+string analysis_names[] = {"ELA", "LG", "AVGDIST", "HSV", "LAB"};
+
+void run_analysis(Mat &src, Mat &dst, analysis_type type, vector<int> params, string output_stem, ptree &root, bool output, bool display, bool verbose) {
+	if(verbose) {
+		cout << "DEBUG: " << analysis_names[type] << " Starting..." << endl;
+		system("pause");
+	}
+
+	string output_filepath;
+	string title;
+	string ptree_element;
+
+	switch(type) {
+		case A_ELA:
+			output_filepath = output_stem + "_ela.png";
+			title = "Error Level Analysis";
+			ptree_element = "ela";
+			error_level_analysis(src, dst, params[0]);
+			break;
+		case A_LG:
+			output_filepath = output_stem + "_lg.png";
+			title = "Luminance Gradient";
+			ptree_element = "lg";
+			luminance_gradient(src, dst);
+			break;
+		case A_AVGDIST:
+			output_filepath = output_stem + "_avgdist.png";
+			title = "Average Distance";
+			ptree_element = "avgdist";
+			average_distance(src, dst);
+			break;
+		case A_HSV:
+			output_filepath = output_stem + "_hsv.png";
+			title = "HSV Histogram";
+			ptree_element = "hsv";
+			hsv_histogram(src, dst, params[0]);
+			break;
+		case A_LAB:
+			output_filepath = output_stem + "_lab.png";
+			title = "Lab Histogram";
+			ptree_element = "lab";
+			lab_histogram_fast(src, dst);
+			break;
+	}
+
+	if(output) {
+		imwrite(output_filepath, dst);
+		string filepath = canonical(output_filepath).make_preferred().string();
+		root.put(ptree_element + ".filename", filepath);
+	}
+	
+	if(display) {
+		namedWindow(title);
+		imshow(title, dst);
+	} else {
+		dst.release();
+	}
+
+	if(verbose) {
+		cout << "DEBUG: " << analysis_names[type] << " Finished." << endl;
+		system("pause");
+	}
+}
+
 int main(int argc, char *argv[]) {
 	//declare program options
 	options_description desc("USAGE: phoenix -f <path_to_file> [options]\nAllowed options");
@@ -62,35 +127,37 @@ int main(int argc, char *argv[]) {
 			cout << desc << endl;
 			return 0;
 		}
-		notify(vm);
-	} catch (const exception &e) {
-		cout << "Erorz!" << endl;
+		notify(vm); //send commands to variables_map
+	} catch (const exception &e) { //error with command options
+		cout << "Error: Cannot parse program commands!" << endl;
 		cout << e.what() << endl;
 		return 1;
 	} catch(...) {
-		cout << "FAK" << endl;
+		cout << "Error: Fatal error while parsing options." << endl;
 		return 1;
 	}
 
+	//some path info
 	path source_path;
 	path output_path;
 	Mat source_image;
 
 	try { //check and try to open source image file (-f)
 		if(!exists(vm["file"].as<string>())) {
-			cout << "no file" << endl;
+			cout << "Error: File not found!" << endl;
 			return 1;
 		}
 
+		//load image to memory
 		source_image = imread(vm["file"].as<string>(), CV_LOAD_IMAGE_COLOR);
 		if(source_image.data == NULL) {
-			cout << "image cant be read" << endl;
+			cout << "Error: Cannot read image!" << endl;
 			return 1;
 		}
 
 		source_path = vm["file"].as<string>();
 		if(vm.count("output") && !is_directory(vm["output"].as<string>())) {
-			cout << "no dir" << endl;
+			cout << "Error: Output directory does not exist!" << endl;
 			return 1;
 		}
 
@@ -98,162 +165,60 @@ int main(int argc, char *argv[]) {
 			output_path = vm["output"].as<string>();
 			output_path = canonical(output_path.make_preferred());
 		}
-	} catch(const exception &e) {
-		cout << "EX: " << e.what() << endl;
+	} catch(const exception &e) { //cannot load the image for some reason
+		cout << "Error: Problem while opening the file!" << endl;
+		cout << e.what() << endl;
 		return 1;
 	}
 
-	if(vm["verbose"].as<bool>()) {
+	ptree root; //property tree for json output in -invoke
+	bool f_display = vm["display"].as<bool>();
+	bool f_verbose = vm["verbose"].as<bool>();
+	bool f_output = vm.count("output");
+
+	string output_stem = output_path.string() + "/" + source_path.stem().string();
+
+	if(f_verbose) {
 		cout << "DEBUG: Image Loaded. Ready to go..." << endl;
 		system("pause");
 	}
 
-	ptree root;
-
 	if(vm.count("ela")) {
-		if(vm["verbose"].as<bool>()) {
-			cout << "DEBUG: ELA Starting..." << endl;
-			system("pause");
-		}
-
 		Mat ela;
-		error_level_analysis(source_image, ela, vm["ela"].as<int>());
-		if(vm.count("output")) {
-			string output_filepath = output_path.string() + "/" + source_path.stem().string() + "_ela.png";
-			imwrite(output_filepath, ela);
+		vector<int> params;
+		params.push_back(vm["ela"].as<int>());
 
-			string filepath = canonical(output_filepath).make_preferred().string();
-			root.put("ela.filename", filepath);
-		}
-		
-		if(vm["display"].as<bool>()) {
-			namedWindow("Error Level Analysis");
-			imshow("Error Level Analysis", ela);
-		} else {
-			ela.release();
-		}
-
-		if(vm["verbose"].as<bool>()) {
-			cout << "DEBUG: ELA Finished." << endl;
-			system("pause");
-		}
+		run_analysis(source_image, ela, A_ELA, params, output_stem, root, f_output, f_display, f_verbose);
 	}
 
 	if(vm["lg"].as<bool>()) {
-		if(vm["verbose"].as<bool>()) {
-			cout << "DEBUG: LG Starting..." << endl;
-			system("pause");
-		}
-
 		Mat lg;
-		luminance_gradient(source_image, lg);
-		if(vm.count("output")) {
-			string output_filepath = output_path.string() + "/" + source_path.stem().string() + "_lg.png";
-			imwrite(output_filepath, lg);
+		vector<int> params;
 
-			string filepath = canonical(output_filepath).make_preferred().string();
-			root.put("lg.filename", filepath);
-		}
-		
-		if(vm["display"].as<bool>()) {
-			namedWindow("Luminance Gradient");
-			imshow("Luminance Gradient", lg);
-		} else {
-			lg.release();
-		}
-
-		if(vm["verbose"].as<bool>()) {
-			cout << "DEBUG: LG Finished." << endl;
-			system("pause");
-		}
+		run_analysis(source_image, lg, A_LG, params, output_stem, root, f_output, f_display, f_verbose);
 	}
 
 	if(vm["avgdist"].as<bool>()) {
-		if(vm["verbose"].as<bool>()) {
-			cout << "DEBUG: Avgdist Starting..." << endl;
-			system("pause");
-		}
-
 		Mat avgdist;
-		average_distance(source_image, avgdist);
-		if(vm.count("output")) {
-			string output_filepath = output_path.string() + "/" + source_path.stem().string() + "_avgdist.png";
-			imwrite(output_filepath, avgdist);
+		vector<int> params;
 
-			string filepath = canonical(output_filepath).make_preferred().string();
-			root.put("avgdist.filename", filepath);
-		}
-		
-		if(vm["display"].as<bool>()) {
-			namedWindow("Average Distance");
-			imshow("Average Distance", avgdist);
-		} else {
-			avgdist.release();
-		}
-
-		if(vm["verbose"].as<bool>()) {
-			cout << "DEBUG: Avgdist Finished." << endl;
-			system("pause");
-		}
+		run_analysis(source_image, avgdist, A_AVGDIST, params, output_stem, root, f_output, f_display, f_verbose);
 	}
 
 	if(vm.count("hsv")) {
-		if(vm["verbose"].as<bool>()) {
-			cout << "DEBUG: HSV Starting..." << endl;
-			system("pause");
-		}
-
 		Mat hsv;
-		hsv_histogram(source_image, hsv, vm["hsv"].as<int>());
+		vector<int> params;
+		params.push_back(vm["hsv"].as<int>());
 
-		if(vm.count("output")) {
-			string output_filepath = output_path.string() + "/" + source_path.stem().string() + "_hsv.png";
-			imwrite(output_filepath, hsv);
-
-			string filepath = canonical(output_filepath).make_preferred().string();
-			root.put("hsv.filename", filepath);
-		}
-		
-		if(vm["display"].as<bool>()) {
-			namedWindow("HSV Histrogram");
-			imshow("HSV Histogram", hsv);
-		} else {
-			hsv.release();
-		}
-
-		if(vm["verbose"].as<bool>()) {
-			cout << "DEBUG: HSV Finished." << endl;
-			system("pause");
-		}
+		run_analysis(source_image, hsv, A_HSV, params, output_stem, root, f_output, f_display, f_verbose);
 	}
 
 	if(vm.count("lab")) {
-		if(vm["verbose"].as<bool>()) {
-			cout << "DEBUG: Lab Starting..." << endl;
-			system("pause");
-		}
-
 		Mat lab;
-		lab_histogram_fast(source_image, lab, vm["lab"].as<int>());
-		if(vm.count("output")) {
-			string output_filepath = output_path.string() + "/" + source_path.stem().string() + "_lab.png";
-			imwrite(output_filepath, lab);
+		vector<int> params;
+		params.push_back(vm["lab"].as<int>());
 
-			string filepath = canonical(output_filepath).make_preferred().string();
-			root.put("lab.filename", filepath);
-		}
-		
-		if(vm["display"].as<bool>()) {
-			namedWindow("Lab Histogram");
-			imshow("Lab Histogram", lab);
-		} else {
-			lab.release();
-		}
-
-		if(vm["verbose"].as<bool>()) {
-			cout << "DEBUG: Lab Finished." << endl;
-			system("pause");
-		}
+		run_analysis(source_image, lab, A_LAB, params, output_stem, root, f_output, f_display, f_verbose);
 	}
 
 	int num_qtables = 0;
