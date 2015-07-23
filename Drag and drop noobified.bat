c3833c422df956fa47bf54b8e590eff8f8cbe32b
henke1010 <henrik@bigshop.se>
2015-07-23 03:09:27 +0200

    Update Drag and drop noobified.bat

--- Drag and drop noobified.bat
+++ Drag and drop noobified.bat
@@ -15,6 +15,9 @@ md "%~dp0output\%TIMESTAMP%"
 
 echo Processing...
 
+@echo off 
+REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\PhotoME.exe"
+if %ERRORLEVEL% EQU 0 (
 IF "%Input%"=="no" ( 
 "%~dp0\phoenix.exe" -f "%~1" --ela --lg --hsv --lab --avgdist -q -o "%~dp0output\%TIMESTAMP%" --copymove
 echo Look at the quality and press space to quit and open file in Photome
@@ -24,15 +27,24 @@ pause >nul
 echo Look at the quality and press space to quit and open file in Photome
 pause >nul
 )
+start "" "C:\Program Files (x86)\PhotoMEBeta\photome.exe" "%~1"
+) ELSE (
+IF "%Input%"=="no" ( 
+"%~dp0\phoenix.exe" -f "%~1" --ela --lg --hsv --lab --avgdist -q -o "%~dp0output\%TIMESTAMP%" --copymove
+echo Look at the quality and press space to quit
+pause >nul 
+) ELSE (
+"%~dp0\phoenix.exe" -f "%~1" --ela --lg --hsv --lab --avgdist -a -q -o "%~dp0output\%TIMESTAMP%" --copymove
+echo Look at the quality and press space to quit
+pause >nul
+)
+)
 
 COPY "%~1 " "%~dp0output\%TIMESTAMP%"
 cls
 
 echo "Done!
 explorer.exe "%~dp0output\%TIMESTAMP%"
-REM start "" "C:\Program Files (x86)\PhotoMEBeta\photome.exe" "%~1"
 
 exit
-
-
 
